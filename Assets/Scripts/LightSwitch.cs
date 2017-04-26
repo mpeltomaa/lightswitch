06df1efcb481c4053107b711501ca3ab30ce5e3a
Mika Peltomaa <mika@peltomaa.org>
2017-04-26 21:15:52 +0300

    Initial commit

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour {
+
+	public bool SwitchOn = false;	// Current state of switch
+	public GameObject[] Lights;		// Array of light objects
+
+	// Move switch to right position
+	void Start() {
+		PositionSwitch ();
+	}
+
+	// If this gameobject is clicked...
+	void OnMouseUp() {
+		// Toggle Switch state
+		SwitchOn = !SwitchOn;
+
+		// Play click sound
+		this.GetComponent<AudioSource> ().Play ();
+
+		// Tilt switch
+		PositionSwitch ();
+
+		// Set lights on / off
+		SetLights ();
+	}
+
+	// To position switch, make 10 degree change on rotation
+	void PositionSwitch() {
+		if (SwitchOn) 
+			transform.rotation = Quaternion.Euler(-85f,0,0);
+		else
+			transform.rotation = Quaternion.Euler(-95f,0,0);
+	}
+
+	// Go thru light objects and toggle light
+	void SetLights() {
+		foreach(GameObject light in Lights) {
+			light.GetComponent<Light> ().enabled = !light.GetComponent<Light> ().enabled;
+		}
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Lightswitch #
+
+Simple lightswitch code to toggle lights on / off. Current version supports only one type of lightswitch. Click sound is from http://soundbible.com/1705-Click2.html
+
+
+### Changelist: ###
+
+1.0: Initial release
+
+### Usage ###
+Open *LightSwitch* scene to see working example.
+
+In example scene you can find *LightSwitch* object, inside *LightSwitch* is *Switch*. Lightswitch code is attached to *Switch*. When you click switch
+code will go thru all lights which are assigned to Lights array on code.
+
+
